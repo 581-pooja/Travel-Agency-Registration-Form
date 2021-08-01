eb116f2b7a56346dd640f23745e35fd57f0aba97
Pooja Bhagat <PoojaBhagat5801@gmail.com>
2021-08-01 22:41:55 +0530

    cleaning the code

--- index.js
+++ index.js
@@ -13,11 +13,9 @@ let validUser = false;
 let validEmail = false;
 let validPhone = false;
 
-// console.log(userName , phone , email);
 
 // Username Event Listener
 userName.addEventListener('blur', ()=>{
-    console.log("Username is blurred");
 
     //  validate name here
     let regex = /^[a-zA-Z]([0-9a-zA-Z]){3,15}$/;
@@ -26,15 +24,12 @@ userName.addEventListener('blur', ()=>{
     // usernamr should not have special characters
 
     let str = userName.value;
-    console.log(str)
     if(regex.test(str)){
-        console.log('it is matched');
         userName.classList.remove('is-invalid')
         userName.classList.add('is-valid');
         validUser = true;
     }
     else{
-        console.log('it is not matched');
         userName.classList.remove('is-valid');
         userName.classList.add('is-invalid');
         validUser = false;
@@ -44,19 +39,15 @@ userName.addEventListener('blur', ()=>{
 
 // Email Event Listener
 email.addEventListener('blur', ()=>{
-    console.log("email is blurred");
     //  validate email here 
     let regex = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+).([a-zA-Z]{2,7}$)/;
     let str = email.value;
-    console.log(str)
     if(regex.test(str)){
-        console.log('it is matched');
         email.classList.remove('is-invalid')
         email.classList.add('is-valid');
         validEmail = true;
     }
     else{
-        console.log('it is not matched');
         email.classList.remove('is-valid');
         email.classList.add('is-invalid')
         validEmail = false;
@@ -66,7 +57,6 @@ email.addEventListener('blur', ()=>{
 
 // phone event listener
 phone.addEventListener('blur', ()=>{
-    console.log("phone is blurred");
     //  validate phone number here
     let regex = /^[0-9]{10}$/;
     // phone number can have only numbers from 1 to 10 digits only
@@ -74,13 +64,11 @@ phone.addEventListener('blur', ()=>{
     let str = phone.value;
     console.log(str)
     if(regex.test(str)){
-        console.log('it is matched');
         phone.classList.remove('is-invalid')
         phone.classList.add('is-valid');
         validPhone = true;
     }
     else{
-        console.log('it is not matched');
         phone.classList.remove('is-valid');
         phone.classList.add('is-invalid');
         validPhone = false;
@@ -92,14 +80,13 @@ phone.addEventListener('blur', ()=>{
 // Submit Button Event listener
 let submit = document.getElementById('submit');
 submit.addEventListener('click',(e)=>{
-    console.log('You clicked the submit button');
     e.preventDefault();
     if(validEmail && validPhone && validUser){
         success.style.display = 'Block';
-        console.log('Phone, email and user are valid. Submitting the form');
+        // console.log('Phone, email and user are valid. Submitting the form');
     }
     else{
         error.style.display = 'Block';
-        console.log('One of Phone, email or user are not valid. Hence not submitting the form. Please correct the errors and try again');
+        // console.log('One of Phone, email or user are not valid. Hence not submitting the form. Please correct the errors and try again');
     }
 })
